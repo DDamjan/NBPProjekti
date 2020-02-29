8b1612bbbf3ccea4d4f04cbc3e47ed9ad78aed4b
DDamjan <d.damjan97@gmail.com>
2020-02-29 03:17:45 +0100

    Ispravljeno nesto sto je primeceno nakon odbrane

--- NGDispatcher/src/app/service/guard/login.guard.ts
+++ NGDispatcher/src/app/service/guard/login.guard.ts
@@ -1,21 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Store } from '@ngrx/store';
-import { selectAllUsers } from 'src/app/store/reducers/user.reducer';
-import { User } from 'src/app/models/User';
-import * as actions from '../../store/actions';
 
 @Injectable({ providedIn: 'root' })
 export class LoginGuard implements CanActivate {
-    currentUser: User;
     constructor(
         private router: Router,
         private store: Store<any>
-    ) {
-        // if (localStorage.getItem('currentUser') != null) {
-        //     // this.store.dispatch(new actions.GetUser(Number(this.currentUser)));
-        // }
-    }
+    ) { }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
         const currentUserType = localStorage.getItem('currentUserType');
@@ -23,7 +15,6 @@ export class LoginGuard implements CanActivate {
             this.router.navigate([`/${currentUserType}/home`]);
             return false;
         }
-        //this.router.navigate(['/login']);
         return true;
     }
 }
